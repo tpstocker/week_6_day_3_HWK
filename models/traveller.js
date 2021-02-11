00fc7214fb458f2d7666f3ac23d4a0ec1ae0e1b1
tpstocker <74962602+tpstocker@users.noreply.github.com>
2021-02-11 08:51:33 +0000

    Add files via upload

---
+++ models/traveller.js
@@ -0,0 +1,43 @@
+const Traveller = function(journeys) {
+  this.journeys = journeys;
+};
+
+Traveller.prototype.getJourneyStartLocations = function() {
+  return this.journeys.map((journey) => {
+    return journey.startLocation;
+  });
+};
+
+Traveller.prototype.getJourneyEndLocations = function () {
+  return this.journeys.map((journey) => {
+    return journey.endLocation;
+  });
+
+};
+
+Traveller.prototype.getJourneysByTransport = function (transport) {
+  return this.journeys.filter((journey) => {
+    return journey.transport === transport;
+  });
+};
+
+Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
+  return this.journeys.every((journey) => {
+    return journey.distance >= distance;
+  });
+};
+
+Traveller.prototype.calculateTotalDistanceTravelled = function () {
+  return this.journeys.reduce((total, journey) => {
+    return total += journey.distance;
+  }, 0);
+};
+
+Traveller.prototype.getUniqueModesOfTransport = function (transport, index) {
+  return this.journeys.every((journey) => {
+    return journey.transport === transport;
+  });
+}; 
+
+
+module.exports = Traveller;
